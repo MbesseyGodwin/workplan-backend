6ceb879c6b6174794e521a6f9f53c370435e912f
mbesseygodwin <mbesseygodwin@gmail.com>
2024-03-30 00:37:11 +0100

    updated refresh token

--- config/Database.js
+++ config/Database.js
@@ -7,26 +7,16 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 
-// Database configuration object to local database
-// const config = {
-//   host: process.env.LOCAL_MYSQL_HOST,
-//   user: process.env.LOCAL_MYSQL_USERNAME,
-//   password: process.env.LOCAL_MYSQL_PASSWORD,
-//   database: process.env.LOCAL_MYSQL_DB_NAME,
-// };
-
-
 // Database configuration object to hosted database
 const config = {
   host: process.env.REMOTE_MYSQL_HOST,
   user: process.env.REMOTE_MYSQL_USERNAME,
   password: process.env.REMOTE_MYSQL_PASSWORD,
-  database: process.env.REMOTE_MYSQL_DB_NAME,
+  database: process.env.REMOTE_MYSQL_DB_NAME
 };
 
 
 // Create a connection pool using the configuration
 const connection = mysql.createPool(config);
-
 // Export the connection pool for use in other files
 module.exports = { connection };
